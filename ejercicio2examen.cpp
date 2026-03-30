d0be9ddc7f190e7ce0a7c96c7564ccfbe6596667
sofiiii07207 <vivianasofiafeijoo@hotmail.com>
2026-03-30 07:23:21 -0500

    Update ejercicio2examen.cpp

--- ejercicio2examen.cpp
+++ ejercicio2examen.cpp
@@ -1,1 +1,16 @@
+#include<iostream>
+using namespace std;
 
+int main() {
+    int n;
+    
+cout<<"Ingrese el número:";
+cin>>n;
+
+for (int i=1; i<=n ; i++){
+    cout<<i<<"→"<<i*i<<endl;
+    
+return 0;
+}
+
+/*Mi programa si corrio y esta copiado tal cual el examen./*

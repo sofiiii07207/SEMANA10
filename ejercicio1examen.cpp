bb55e947d73683c792661c9c29eb200bdd3550f8
sofiiii07207 <vivianasofiafeijoo@hotmail.com>
2026-03-30 07:17:40 -0500

    Update ejercicio1examen.cpp

--- ejercicio1examen.cpp
+++ ejercicio1examen.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
+using namespace std;
 
+int main() {
+    int a,b;
+
+cout<<"Ingrese el primer número:";
+cin>> a;
+
+cout<<"Ingrese el segundo número:";
+cin>> b;
+
+cout<<"Suma:"<<a+b<<endl;
+cout<<"Resta:"<<a-b<<endl;
+cout<<"Multiplicación"<<a*b<<endl;
+
+return 0;
+}
+
+/*Mi programa corrió a la perfección y lo copie tal cual como estaba en el examen.*/
